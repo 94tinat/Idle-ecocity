43d66883bf9edc5d2c878d1713d28a464c779a90
Valentina Tosto <>
2020-07-22 13:18:54 +0200

    UI structure + incremental coins

---
+++ Idle ecocity/Assets/IdleManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class IdleManager : MonoBehaviour
+{
+    //Show number of coins earned
+    public Text ecoCoinsText;
+
+    //Represent accurate number of coins
+    public double ecoCoins;
+
+    void Start()
+    {
+        //Initialize coins
+        ecoCoins = 0;
+    }
+
+    void Update()
+    {
+        //Update number of coins earned 
+        ecoCoinsText.text = "Coins:  " + ecoCoins;
+    }
+
+    //Method to earn by furniture
+    public void ClickToIncrease()
+    {
+        ecoCoins += 1;
+    }
+}
